610eb7f556aaee354e24f658174f8a9897446daa
Rick Huang <rick.huang@silkrode.com.tw>
2019-10-21 12:19:30 +0800

    Initial Commit

---
+++ kbManagerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kbManagerDemo
+//
+//  Created by 黃柏叡 on 2019/10/21.
+//  Copyright © 2019 黃柏叡. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
